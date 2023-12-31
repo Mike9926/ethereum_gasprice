0f628960a789a45cbca74a18c03753a5a11988ae
Mima9926 <mikechapuma@gmail.com>
2023-12-31 02:26:43 +0200

    fetch and refresh

--- gas_tracker/gasprice/urls.py
+++ gas_tracker/gasprice/urls.py
@@ -3,13 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Home page
     path("home/", views.home, name="home"),
-    # About page
     path("about/", views.about, name="about"),
-    # Contact page
     path("contact/", views.contact, name="contact"),
-    # Gas price detail page
-    path("<int:pk>/detail", views.gas_price_detail, name="gas_price_detail"),
+    path('gas-price/<int:id>/', views.gas_price_detail, name='gas_price_detail'),
     path('fetch_gas_prices/', views.fetch_gas_prices, name='fetch_gas_prices'),
+    path('update-gas-price/', views.update_gas_price, name='update-gas-price'),
+
 ]
--- gas_tracker/gasprice/views.py
+++ gas_tracker/gasprice/views.py
@@ -1,9 +1,10 @@
 # views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from .models import GasPrice
 #import my script
 from django.core.management import call_command # Import the call_command functi
+from .management.commands.fetch_gas_prices import Command
 
 
 # Home view
@@ -28,7 +29,8 @@ def home(request):
         "gas_prices": gas_prices
     }
     # Return a render function call with the request, the template name, and the contexts
-    return render(request, "gasprice/home.html", context)
+    return render(request, "gasprice/index.html", context)
+
 def fetch_gas_prices(request):
     # Run your script to fetch and store gas prices
     fetch_gas_prices()
@@ -42,6 +44,8 @@ def fetch_gas_prices(request):
     }
     # Return a JSON response with the data
     return JsonResponse(data)
+
+
 # About view
 def about(request):
     # Return a render function call with the request and the template names
@@ -53,12 +57,19 @@ def contact(request):
     return render(request, "gasprice/contact.html")
 
 # Gas price detail view
-def gas_price_detail(request, pk):
-    # Get the gas price object from the database
-    gas_price = GasPrice.objects.get(pk=pk)
-    # Create a context dictionary with the gas price data
-    context = {
-        "gas_price": gas_price
+def gas_price_detail(request, id):
+    # Get the gas price object that matches the id or raise a 404 error if not found
+    gas_price = get_object_or_404(GasPrice, id=id)
+    # Render the template with the gas price object as context
+    return render(request, 'gasprice/gas_price_detail.html', {'gas_price': gas_price})
+
+def update_gas_price(request):
+    command = Command()
+    command.handle()
+    latest_gas_price = GasPrice.objects.latest('timestamp')
+    data = {
+        'gas_price': latest_gas_price.gas_price,
+        'transaction_speed': latest_gas_price.transaction_speed,
+        'timestamp': latest_gas_price.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
     }
-    # Return a render function call with the request, the template name, and the contexts
-    return render(request, "gasprice/gas_price_detail.html", context)
+    return JsonResponse(data)
